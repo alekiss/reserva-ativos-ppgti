91a21113c8e35874fd8ccb5ccd18a54b13fc0cbe
alekiss <alekiss.melo@gmail.com>
2025-05-29 03:58:05 -0300

    fix: correções para subir o deploy

--- src/pages/minhas-reservas/minhas-reservas.tsx
+++ src/pages/minhas-reservas/minhas-reservas.tsx
@@ -1,32 +1,32 @@
 import { useEffect, useState } from "react";
 import { Container, Typography, Dialog, DialogTitle, DialogActions, Button } from "@mui/material";
 import { Reserva } from "../../types/reserva";
-import ReservaItem from "../../components/reserva-item/reserva-item";
+// import ReservaItem from "../../components/reserva-item/reserva-item";
 
-const reservasMock: Reserva[] = [
-  {
-    id: 1,
-    data: "2023-10-01",
-    horaInicio: "10:00",
-    horaFim: "12:00",
-    tipo: "Sala de Reunião",
-    local: "Sala A",
-    imagem: "https://www.iq.harvard.edu/sites/projects.iq.harvard.edu/files/styles/os_files_xlarge/public/harvard-iqss/files/k301_01.png?m=1714725215&itok=IGS1ojuR"
-  },
-  {
-    id: 2,
-    data: "2023-10-02",
-    horaInicio: "14:00",
-    horaFim: "16:00",
-    tipo: "Auditório",
-    local: "Auditório Principal",
-    imagem: "https://www.iq.harvard.edu/sites/projects.iq.harvard.edu/files/styles/os_files_xlarge/public/harvard-iqss/files/k301_01.png?m=1714725215&itok=IGS1ojuR"
-  }
-];
+// const reservasMock: Reserva[] = [
+//   {
+//     id: 1,
+//     data: "2023-10-01",
+//     horaInicio: "10:00",
+//     horaFim: "12:00",
+//     tipo: "Sala de Reunião",
+//     local: "Sala A",
+//     imagem: "https://www.iq.harvard.edu/sites/projects.iq.harvard.edu/files/styles/os_files_xlarge/public/harvard-iqss/files/k301_01.png?m=1714725215&itok=IGS1ojuR"
+//   },
+//   {
+//     id: 2,
+//     data: "2023-10-02",
+//     horaInicio: "14:00",
+//     horaFim: "16:00",
+//     tipo: "Auditório",
+//     local: "Auditório Principal",
+//     imagem: "https://www.iq.harvard.edu/sites/projects.iq.harvard.edu/files/styles/os_files_xlarge/public/harvard-iqss/files/k301_01.png?m=1714725215&itok=IGS1ojuR"
+//   }
+// ];
 
 const MinhasReservas = () => {
-  const [reservas, setReservas] = useState<Reserva[]>(reservasMock);
-  const [reservaSelecionada, setReservaSelecionada] = useState<Reserva | null>(null);
+  // const [reservas, setReservas] = useState<Reserva[]>(reservasMock);
+  const [reservaSelecionada] = useState<Reserva | null>(null); //Adicionar depois o setReservaSelecionada
   const [modalCancelarAberto, setModalCancelarAberto] = useState(false);
 
   const carregarReservas = async () => {
@@ -34,10 +34,10 @@ const MinhasReservas = () => {
     // setReservas(data);
   };
 
-  const handleCancelar = (reserva: Reserva) => {
-    setReservaSelecionada(reserva);
-    setModalCancelarAberto(true);
-  };
+  // const handleCancelar = (reserva: Reserva) => {
+  //   setReservaSelecionada(reserva);
+  //   setModalCancelarAberto(true);
+  // };
 
   const confirmarCancelamento = async () => {
     if (reservaSelecionada) {
@@ -47,10 +47,10 @@ const MinhasReservas = () => {
     }
   };
 
-  const handleEditar = (reserva: Reserva) => {
-    // Exibir painel lateral ou navegação para edição
-    console.log("Editar reserva:", reserva);
-  };
+  // const handleEditar = (reserva: Reserva) => {
+  //   // Exibir painel lateral ou navegação para edição
+  //   console.log("Editar reserva:", reserva);
+  // };
 
   useEffect(() => {
     carregarReservas();
@@ -59,14 +59,14 @@ const MinhasReservas = () => {
   return (
     <Container maxWidth="md">
       <Typography variant="h5" gutterBottom>Minhas Reservas</Typography>
-      {reservas.map((reserva) => (
+      {/* {reservas.map((reserva) => (
         <ReservaItem
           key={reserva.id}
           reserva={reserva}
           onCancelar={handleCancelar}
           onEditar={handleEditar}
         />
-      ))}
+      ))} */}
 
       <Dialog open={modalCancelarAberto} onClose={() => setModalCancelarAberto(false)}>
         <DialogTitle>Confirmar cancelamento?</DialogTitle>
